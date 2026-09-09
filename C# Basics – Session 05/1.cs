9041a373ff1428a55dff5a4a4b287c444467dd73
kapil <mohamedkapil31@gmail.com>
2026-09-10 01:43:05 +0300

    1 question

---
+++ C# Basics – Session 05/1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Basics___Session_05
+{
+    internal class Book
+    {
+        private string password = "secret";
+    }
+}
--- C# Basics – Session 05/Program.cs
+++ C# Basics – Session 05/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            
+            #region 1 question
+
+            Book book = new Book();
+     
+            Console.WriteLine(book.password);
+
+            #endregion
+
+            #region 2 question
+            #endregion
+
+            #region 3 question
+            #endregion
+
+            #region 4 question
+            #endregion
+
+            #region 5 question
+            #endregion
+
+            #region 6 question
+            #endregion
+
+            #region 7 question
+            #endregion
+
+            #region 8 question
+            #endregion
+
+            #region 9 question
+            #endregion
+
+            #region 10 question
+            #endregion
+
         }
     }
 }
